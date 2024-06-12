fa1c1d420cb4d1d9c7a243430702a11b0a9b1676
alineyu <yuka_kokubu@hotmail.com>
2024-06-12 09:47:05 -0300

    ajustes mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]')
-   
+    const questions = document.querySelectorAll('[data-faq-question]')
 
+    const heroSection = document.querySelector('.hero')
+    const heroHeight = heroSection.clientHeight
+
+window.addEventListener('scroll', function() {
+    const posicaoAtual = window.scrollY;
+
+    if(posicaoAtual < heroHeight) {
+        ocultaElementosHeader();
+    } else {
+        exibeElementosHeader();
+    }
+})
+
+    //secao de atracoes, promacao das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,8 +27,28 @@ document.addEventListener('DOMContentLoaded', function() {
             
         })
     }
+
+    //secao FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
 
+function ocultaElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+function exibeElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
+
+function abreOuFechaResposta(element) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = element.target.parentNode;
+    elementoPai.classList.toggle(classe);
+
+}
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
